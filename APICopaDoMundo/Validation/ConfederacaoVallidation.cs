1667cce88db9b8b978f8eec930b7945353864edd
Germano <germanodrp18@gmail.com>
2022-02-18 13:41:01 -0300

    subindo vallidation

---
+++ APICopaDoMundo/Validation/ConfederacaoVallidation.cs
@@ -0,0 +1,28 @@
+﻿using APICopaDoMundo.Models;
+using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace APICopaDoMundo.Validation
+{
+    public class ConfederacaoVallidation:AbstractValidator<Confederacoes>
+    {
+        public ConfederacaoVallidation()
+        {
+            RuleFor(Confederacoes => Confederacoes.Id)
+                .NotEmpty().WithMessage("Campo confederações vazio,tente novamente ")
+                .NotNull().WithMessage("Campo confederações não informado,tente novamente !");
+
+            RuleFor(Confederacoes => Confederacoes.Sigla)
+                .NotEmpty().WithMessage("Campo confederações vazio,tente novamente")
+                .NotNull().WithMessage("Campo confederações não informado,tente novamente !")
+                .MinimumLength(2).WithMessage("Minimo 3 caracteres")
+                .Must(SomenteLetras).WithMessage("Somente Letras");
+
+
+        }
+        public static bool SomenteLetras(string palavra)
+        {
+            return Regex.IsMatch(palavra, @"^[a-zA-Z]+$");
+        }
+    }
+}
---
+++ APICopaDoMundo/Validation/PaisesVallidation.cs
@@ -0,0 +1,40 @@
+﻿using APICopaDoMundo.Models;
+using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace APICopaDoMundo.Validation
+{
+    public class PaisesVallidation: AbstractValidator<Paises>
+    {
+        public PaisesVallidation()
+        {
+            RuleFor(Paises => Paises.Id)
+                .NotEmpty().WithMessage("Campo paises vazio,tente novamente")
+                .NotNull().WithMessage("Campo paises não informado, tente novamente !");
+
+            RuleFor(Paises => Paises.Nome)
+                .NotEmpty().WithMessage("Campo paises vazio,tente novamente")
+                .NotNull().WithMessage("Campo paises não informado, tente novamente !")
+                .MinimumLength(2).WithMessage("Minimo 2 caracteres")
+                .Must(SomenteLetras).WithMessage("Somente Letras");
+
+            RuleFor(Paises => Paises.RankingFifa)
+                .NotEmpty().WithMessage("Campo paises vazio,tente novamente")
+                .NotNull().WithMessage("Campo paises não informado, tente novamente !");
+                
+
+         
+
+        }
+        public static bool SomenteNumero(string Numeros)
+        {
+            //logica da receita
+            return Regex.IsMatch(Numeros, @"^\d$");
+
+        }
+        public static bool SomenteLetras(string palavra)
+        {
+            return Regex.IsMatch(palavra, @"^[a-zA-Z]+$");
+        }
+    }
+}
